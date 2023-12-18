5286153fc6e3be5a31f4bc506d13b520f4308d23
manhhieu120499 <93460932+manhhieu120499@users.noreply.github.com>
2023-12-18 10:45:36 +0700

    push code

--- index.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.get("/", (req, res) => res.send("Hello world!"));
-app.listen(port, () => console.log(`Example for listening at http://localhost:${port}`));
